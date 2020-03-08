45f938b89695b7d89e6c4ab15468da401446ce6e
Shtokarev Yuriy <yuri.shtokarev@fusion-team.com>
2020-03-08 21:14:19 +0300

    Added Redis and logger

---
+++ src/utils/init-redis.ts
@@ -0,0 +1,50 @@
+import * as Redis from 'redis';
+import { promisify } from 'util';
+
+import logger from './logger';
+
+export interface RedisFactoryProps {
+  host: string;
+}
+
+export interface AsyncRedisClient extends Redis.RedisClient {
+  getAsync: (key: string) => Promise<string>;
+  setAsync: (key: string, value: string) => Promise<unknown>;
+  setexAsync: (key: string, seconds: number, value: string) => Promise<string>;
+  saddAsync: (key: string, value: string) => Promise<number>;
+  sremAsync: (key: string, value: string) => Promise<number>;
+  smembersAsync: (key: string) => Promise<string[]>;
+}
+
+let redisClient: AsyncRedisClient;
+
+export async function initRedisClient({ host }: RedisFactoryProps): Promise<AsyncRedisClient> {
+  if (redisClient) {
+    return redisClient;
+  }
+
+  redisClient = Redis.createClient({ host }) as AsyncRedisClient;
+
+  redisClient.getAsync = promisify(redisClient.get).bind(redisClient);
+  redisClient.setAsync = promisify(redisClient.set).bind(redisClient);
+  redisClient.setexAsync = promisify(redisClient.setex).bind(redisClient);
+  redisClient.saddAsync = promisify(redisClient.sadd).bind(redisClient);
+  redisClient.sremAsync = promisify(redisClient.srem).bind(redisClient);
+  redisClient.smembersAsync = promisify(redisClient.smembers).bind(redisClient);
+
+  enableRedisAutoReconnect(redisClient);
+
+  logger.log(`redis connected: ${host}`);
+
+  return redisClient;
+}
+
+function enableRedisAutoReconnect(client: AsyncRedisClient) {
+  if (!client || !client.on) {
+    return;
+  }
+
+  client.on('error', (error: unknown) => {
+    logger.error('Redis client error', error);
+  });
+}
---
+++ src/utils/logger.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Console {
+  error: Function;
+  log: Function;
+  warn: Function;
+}
+
+const dumpFunction: Function = () => { /*-empty-*/ };
+
+const logger = {
+  actingConsole: {
+    error: dumpFunction,
+    log: dumpFunction,
+    warn: dumpFunction,
+  } as Console,
+
+  init: (console: { error: Function; log: Function; warn: Function }) => {
+    logger.actingConsole = console;
+    return logger;
+  },
+  error: (...x: any) => logger.actingConsole.error(...x),
+  log: (...x: any) => logger.actingConsole.log(...x),
+  warn: (...x: any) => logger.actingConsole.warn(...x),
+};
+
+export default logger;
