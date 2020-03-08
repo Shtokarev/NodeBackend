b8da40b63fe932615f6ef0494cce4ce0a66abfd7
Shtokarev Yuriy <yuri.shtokarev@fusion-team.com>
2020-03-08 21:10:57 +0300

    Сonfigured environment

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+    jasmine: true,
+    node: true,
+    commonjs: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "off",
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
+    "semi": ["warn", "always"],
+    "quotes": ["error", "single"],
+  },
+};
