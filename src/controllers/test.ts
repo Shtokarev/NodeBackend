1f47ea6794727db8c6533089dcc214e91b90e5c2
Shtokarev Yuriy <yuri.shtokarev@fusion-team.com>
2020-03-09 17:08:09 +0300

    test36

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -1,19 +1,8 @@
 import { Request, Response } from 'express';
-import {
-  PORT,
-  REDIS_HOST,
-  MONGODB_CONNECTION_STRING,
-} from '../utils/env-loader';
-
 
 import logger from '../utils/logger';
 
 export const test = (req: Request, res: Response) => {
   logger.log('incoming GET on route /test');
-  const result = {
-    PORT,
-    REDIS_HOST,
-    MONGODB_CONNECTION_STRING,
-  };
-  res.json(result);
+  res.send('Hello world!');
 };
